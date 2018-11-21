f68f478839d9892f36b88a9367abf18bf4ece279
Marius van Niekerk <marius.v.niekerk@gmail.com>
2018-11-21 09:50:08 -0500

    Solve noarch and arch specific jointly for graph structure

--- graph.py
+++ graph.py
@@ -10,14 +10,16 @@ from pandas.io import json
 from cachetools import LRUCache, cachedmethod, TTLCache
 
 
-def build_repodata_graph(repodata):
+def build_repodata_graph(repodata, arch):
     G = networkx.DiGraph()
     for p, v in repodata['packages'].items():
         name = v['name']
         G.add_node(name)
+        G.nodes[name].setdefault('arch', set())
+        G.nodes[name]['arch'].add(arch)
 
-        G.nodes[name].setdefault('packages', {})
-        G.nodes[name]['packages'][p] = v
+        G.nodes[name].setdefault(f'packages_{arch}', {})
+        G.nodes[name][f'packages_{arch}'][p] = v
 
         for dep in v['depends']:
             dep_name, _, _ = dep.partition(' ')
@@ -36,6 +38,7 @@ def recursive_parents(G: networkx.DiGraph, nodes):
         n = todo.popleft()
         if n in done:
             continue
+        # TODO: this seems to cause issues with root nodes like zlib
         children = list(G.predecessors(n))
         todo.extend(children)
         done.add(n)
@@ -48,7 +51,8 @@ class RawRepoData:
     def __init__(self, channel, arch='linux-64'):
         data = requests.get(f'https://conda.anaconda.org/{channel}/{arch}/repodata.json.bz2')
         repodata = json.loads(bz2.decompress(data.content))
-        self.graph = build_repodata_graph(repodata)
+        self.arch = arch
+        self.graph = build_repodata_graph(repodata, arch)
 
 
 def get_repo_data(channel, arch):
@@ -64,12 +68,17 @@ class ArtifactGraph:
 
     def __init__(self, channel, arch, constraints):
         self.raw = get_repo_data(channel, arch)
-        self.constrain_graph(self.raw.graph, constraints)
-        self.cache = TTLCache(10, ttl=60)
+        self.arch = arch
+        self.noarch = get_repo_data(channel, 'noarch')
+        self.constrain_graph(self.raw.graph, self.noarch.graph, constraints)
+        self.cache = TTLCache(100, ttl=600)
 
-    def constrain_graph(self, graph, constraints):
+    def constrain_graph(self, graph, noarch_graph, constraints):
+        # Since noarch is solved along with our normal channel we need to combine the two for our effective
+        # graph.
+        combined_graph = networkx.compose(graph, noarch_graph)
         if constraints:
-            nodes = recursive_parents(graph, constraints)
+            nodes = recursive_parents(combined_graph, constraints)
             subset = self.raw.graph.subgraph(nodes)
             self.constrained_graph = subset
         else:
@@ -78,7 +87,7 @@ class ArtifactGraph:
     def repodata_json_dict(self):
         packages = {}
         for n in self.constrained_graph:
-            packages.update(self.constrained_graph.nodes[n].get('packages', {}))
+            packages.update(self.constrained_graph.nodes[n].get(f'packages_{self.arch}', {}))
         return {'packages': packages}
 
     @cachedmethod(operator.attrgetter('cache'))
