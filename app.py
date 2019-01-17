b0152750cf313eaf89fb0f116e8fb92a5e69d463
christopher <cjwright4242@gmail.com>
2019-01-16 20:34:40 -0500

    bumped version to 0.0.9

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ logger = logging.getLogger(__name__)
 app = Flask(__name__)
 arch = ['linux-64', 'noarch', 'osx-64']
 
-VERSION = "0.0.8"
+VERSION = "0.0.9"
 CHANNEL_MAP = {
     'conda-forge': 'https://conda-static.anaconda.org/conda-forge',
 }
