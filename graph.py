7f1dedd4186ade7b34e2391ca3330677e6ca5ede
Marius van Niekerk <marius.v.niekerk@gmail.com>
2019-04-05 22:34:23 -0500

    typo fixup

--- graph.py
+++ graph.py
@@ -390,7 +390,7 @@ def get_artifact_graph(
         if arch == "win-64":
             new_channel = [
                 "https://repo.anaconda.com/pkgs/main",
-                "https://repo.anaconda.com/pkgs/msys"
+                "https://repo.anaconda.com/pkgs/msys",
                 "https://repo.anaconda.com/pkgs/r",
             ]
         else:
